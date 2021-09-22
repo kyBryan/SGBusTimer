73ef95d9309638951ee4fda2edd4e4fccaee4de5
Lee Kim Yang Bryan <lee.kim.yang.bryan@accenture.com>
2021-09-22 19:33:36 +0800

    Added Google Map with restriction to this application only

--- app/src/main/java/com/acn/sgbustimer/controller/BusNearbyFragment.kt
+++ app/src/main/java/com/acn/sgbustimer/controller/BusNearbyFragment.kt
@@ -8,9 +8,17 @@ import com.acn.sgbustimer.R
 import com.acn.sgbustimer.databinding.BusNearbyViewFragmentBinding
 import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 
-class BusNearbyFragment : Fragment() {
+class BusNearbyFragment : Fragment(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,8 +26,12 @@ class BusNearbyFragment : Fragment() {
     ): View? {
         val binding = DataBindingUtil.inflate<BusNearbyViewFragmentBinding>(inflater, R.layout.bus_nearby_view_fragment, container, false)
 
-
         setHasOptionsMenu(true)
+
+//        val mapFragment = supportFragmentManager
+//            .findFragmentById(R.id.map) as SupportMapFragment
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
         // Inflate the layout for this fragment
         return binding.root
@@ -37,5 +49,14 @@ class BusNearbyFragment : Fragment() {
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
