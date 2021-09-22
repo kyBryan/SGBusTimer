1a744f9c44a7b2851c838dc22655dba05d68005a
Lee Kim Yang Bryan <lee.kim.yang.bryan@accenture.com>
2021-09-22 11:02:42 +0800

    Restructured the navigation drawer to incorporate with navigation graph.

--- app/src/main/java/com/acn/sgbustimer/controller/BusNearbyFragment.kt
+++ app/src/main/java/com/acn/sgbustimer/controller/BusNearbyFragment.kt
@@ -7,20 +7,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.acn.sgbustimer.R
-import com.acn.sgbustimer.databinding.BusViewFragmentBinding
+import com.acn.sgbustimer.databinding.BusNearbyViewFragmentBinding
 
 
-class BusViewFragment : Fragment() {
+class BusNearbyFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        val binding = DataBindingUtil.inflate<BusViewFragmentBinding>(inflater, R.layout.bus_view_fragment, container, false)
+        val binding = DataBindingUtil.inflate<BusNearbyViewFragmentBinding>(inflater, R.layout.bus_nearby_view_fragment, container, false)
 
 
         // Inflate the layout for this fragment
         return binding.root
     }
+
 }
