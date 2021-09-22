0cd8a3a05b188a6d1fce6bbcd4d28641eeb012af
Lee Kim Yang Bryan <lee.kim.yang.bryan@accenture.com>
2021-09-22 13:41:36 +0800

    Fixed Navigation Drawer and included a close button

--- app/src/main/java/com/acn/sgbustimer/MainActivity.kt
+++ app/src/main/java/com/acn/sgbustimer/MainActivity.kt
@@ -2,8 +2,12 @@ package com.acn.sgbustimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.MenuItem
+import android.widget.ImageButton
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
@@ -11,19 +15,32 @@ import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.acn.sgbustimer.databinding.ActivityMainBinding
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var appBarConfiguration : AppBarConfiguration
+    private lateinit var navController : NavController
+    private lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Timber.plant(Timber.DebugTree())
+
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         drawerLayout = binding.drawerLayout
-        val navController = this.findNavController(R.id.navHostFragment)
-        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        navController = this.findNavController(R.id.navHostFragment)
+        //NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
         appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        toolbar = binding.toolbar
+
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
         // prevent nav gesture if not on start destination
         navController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, bundle: Bundle? ->
             if (nd.id == nc.graph.startDestination) {
@@ -33,12 +50,21 @@ class MainActivity : AppCompatActivity() {
             }
         }
         NavigationUI.setupWithNavController(binding.navView, navController)
+
+        // setup navigation close button
+        val navCloseBtn = binding.navView.getHeaderView(0).findViewById<ImageButton>(R.id.navCloseBtn)
+        navCloseBtn.setOnClickListener(){
+            Timber.i("Navigation Drawer Close Button Pressed")
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }
     }
 
 
+
     override fun onSupportNavigateUp(): Boolean {
+        Timber.i("Enter Support Navigation Up")
         val navController = this.findNavController(R.id.navHostFragment)
         return NavigationUI.navigateUp(navController, appBarConfiguration)
-
+//        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
